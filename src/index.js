e490ed1f29220175903190f0527a8c6b55c487c9
alanltr <letrocqueralan@gmail.com>
2021-06-01 01:16:30 +0200

    mise en place du store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 // == Import : npm
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from 'src/store';
 
 // == Import : local
 import App from 'src/components/App';
 
 // == Render
-const rootReactElement = <App />;
+const rootReactElement = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 const target = document.getElementById('root');
 
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from 'src/reducers';
+
+const enhancers = composeWithDevTools(
+  applyMiddleware(
+  ),
+);
+
+const store = createStore(
+  reducer,
+  enhancers,
+);
+
+export default store;
